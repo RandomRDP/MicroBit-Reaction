c6adf59c54457b357f59e04402161eaaf7a70206
RandomRDP <35812330+RandomRDP@users.noreply.github.com>
2021-03-27 23:21:46 +0000

    Replaced tabs with spaces

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ MAX_TIME = const(10 * 1000)
 
 while True:
 
-	while not button_a.is_pressed() or not button_b.is_pressed():
 		pass
+    while not button_a.is_pressed() or not button_b.is_pressed():
     
     for i in range(3, 0, -1):
         music.play( ["b5:1"] )       
         display.show("{}".format(i))
         utime.sleep(1)
 	
-    display.clear()
+    display.clear()    
     music.play( music.BA_DING )
     
     utime.sleep_ms(randint(MIN_TIME, MAX_TIME))
