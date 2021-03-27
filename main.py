74f8c4de0ed5533b916587ecc7c9fc44caff2fc1
RandomRDP <35812330+RandomRDP@users.noreply.github.com>
2021-03-27 22:44:29 +0000

    Improved countdown

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ while True:
 	while not button_a.is_pressed() or not button_b.is_pressed():
 		pass
     
-    music.play( ["b5:1"] )       
-    display.show("3")
-    utime.sleep(1)
-    music.play( ["b5:1"] )   
-    display.show("2")
-    utime.sleep(1)
-    music.play( ["b5:1"] )      
-    display.show("1")
-    utime.sleep(1)
+    for i in range(3, 0, -1):
+        music.play( ["b5:1"] )       
+        display.show("{}".format(i))
+        utime.sleep(1)
 	
     display.clear()
     music.play( music.BA_DING )
